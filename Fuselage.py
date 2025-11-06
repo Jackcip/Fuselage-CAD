03105625bbcdbafa730686731abd2eab41bf68a5
JacopoVetrano <jacopo.vetrano@gmail.com>
2025-11-06 21:07:31 +0100

    Add files via upload

--- Fuselage.py
+++ Fuselage.py
@@ -1,37 +1,53 @@
 import cadquery as cq
 import numpy as np
 
-rad = 10
-h_ell = 25
-length = 40
-angle = float(360.0)
-thickness = 1
+rad = 10        
+h_ell = 25      
+length = 40     
+thickness = 1   
 
-left_dome_sketch = (cq.Workplane("XZ")
-           .ellipseArc(rad, h_ell, 0, 90,startAtCurrent = False)
-           .lineTo(0,0)
-           .close())
-left_dome = left_dome_sketch.revolve(angle).faces("<Z").shell(-thickness, kind = "arc")
-body = (cq.Workplane("XY")
-      .circle(rad)
-      .extrude(-length)
-      .faces("|Z")
-      .shell(-thickness)
-      )
+def half_ellipse_points(rx, ry, top=True, n=20):
+    if top:
+        t = np.linspace(0, np.pi/2, n)  
+    else:
+        t = np.linspace(-np.pi/2, 0, n) 
+    return [(rx * np.cos(a), ry * np.sin(a)) for a in t]
 
-right_dome_sketch = (cq.Workplane("XZ")
-           .move(0,-length)
-           .lineTo(0,-h_ell-length)          
-           .ellipseArc(rad, h_ell, 270,360,startAtCurrent = True)
-           .close()
-           )
-right_dome = (right_dome_sketch
-              .revolve(angle)
-              .faces("|Z")
-              .shell(-thickness)
-              )
 
-show_object([right_dome,body,left_dome])
 
-combined = left_dome.union(body).union(right_dome)
-cq.exporters.export(combined, 'Fuselage.step')
+
+outer_pts = half_ellipse_points(rad, h_ell, top=True)
+
+inner_pts = half_ellipse_points(rad - thickness, h_ell - thickness, top=True)
+
+sketch = (
+    cq.Workplane("XZ")
+    .spline(outer_pts)
+     .lineTo(0, 0)
+     .moveTo(0, 0)
+     .lineTo(0, h_ell - thickness)
+     .spline(list(reversed(inner_pts)))
+     .close()
+)
+left_dome = sketch.revolve(360)
+
+
+outer_body = (
+    cq.Workplane("XY")
+    .circle(rad)
+    .extrude(-length)
+)
+inner_body = (
+    cq.Workplane("XY")
+    .circle(rad - thickness)
+    .extrude(-length)
+)
+body = outer_body.cut(inner_body)
+
+right_dome = left_dome.mirror(mirrorPlane="XY").translate((0, 0, -length))
+
+
+fuselage = body.union(left_dome).union(right_dome)
+
+show_object(fuselage)
+cq.exporters.export(fuselage, "Fuselage.step", cq.exporters.ExportTypes.STEP)
