f5e642343249925b93c4fc6a61ab9fa3f76c5ef8
JacopoVetrano <jacopo.vetrano@gmail.com>
2026-03-02 22:45:53 +0100

    Add files via upload

--- Fuselage.py
+++ Fuselage.py
@@ -2,10 +2,14 @@ import cadquery as cq
 import numpy as np
 
 # PARAMETERS
-rad = 25
-h_ell = 25
-length = 40
-thickness = 1
+rad = 80    #raggio fusoliera
+h_ell = 80  #semiasse maggiore calotta
+length = 750    #corpo centrale
+thickness = 1   #parete fusoliera
+fl_pos = -10    #(mm)
+fl_thickness = 1    #(mm)
+den_fuselage = 200  
+den_floor = 1
 
 # POINTS FOR ELLIPSE SPLINE
 def half_ellipse_points(rx, ry, n=20):
@@ -45,8 +49,48 @@ body = outer_body.cut(inner_body)
 # DOME MIRROR
 right_dome = left_dome.mirror(mirrorPlane="XY").translate((0, 0, -length))
 
-# FUSELAGE ASSEMBLY
-fuselage = body.union(left_dome).union(right_dome)
+# FUSELAGE ASSEMBLY (WITHOUT FLOOR)
+asm = body.union(left_dome).union(right_dome)
+
+
+sketch_cut = (
+    cq.Workplane("XZ")
+    .spline(inner_pts)
+     .lineTo(0, 0)
+     .close()
+     )
+
+dome_cut = sketch_cut.revolve(360)
+dome_mirr_cut = dome_cut.mirror(mirrorPlane="XY").translate((0, 0, -length))
+
+asm_cut= dome_cut.union(dome_mirr_cut).union(inner_body)
+
+
+#FLOOR
+fl_length = length+h_ell+h_ell
+fl_width = rad*2
+
+floor = (cq.Workplane("XZ")
+         .box(fl_width,fl_length,fl_thickness)
+         .translate((0,fl_pos,-length/2))
+         .intersect(asm_cut)
+    )
+
+#COMPLETE FUSELAGE ASSEMBLY
+fuselage = asm.union(floor)
 
 # EXPORT FILE
-cq.exporters.export(fuselage, "Fuselage.step")
+# Choose "floor" or "fuselage" or "asm"(fuselage w/o floor)
+cq.exporters.export(asm, "fuselage_prova.step")
+
+con= 1e-9
+vol_fuselage = (asm.val().Volume())*con
+vol_floor = (floor.val().Volume())*con
+
+
+mass_fuselage = vol_fuselage * den_fuselage
+mass_floor = vol_floor * den_floor
+
+tot_mass = mass_fuselage
+
+print(tot_mass)
